2aa87bce24a6f5fce0f8759df17bfa0ff32639d2
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2020-12-08 21:04:55 -0300

    feat: adds basic model

--- model/nmf.py
+++ model/nmf.py
@@ -1,6 +1,36 @@
 from gensim.models.nmf import Nmf
 
+RANDOM_STATE=1
+
 class NmfModel:
 
-    def __init__(self, corpus, n_topics, id2word=None):
-        self.corpus = corpus
+    def __init__(
+            self, corpus, num_topics, dictionary=None, 
+            chunksize=1000, passes=200, kappa=1.0):
+        self._corpus = corpus
+        self._num_topics = num_topics
+        self._dictionary = dictionary
+
+        self._model = Nmf(
+                corpus, num_topics=num_topics, 
+                id2word=dictionary, chunksize=chunksize,
+                passes=passes, kappa=kappa,
+                random_state=RANDOM_STATE)
+
+    @property
+    def model(self):
+        return self._model
+
+    def get_document_topics(self, document):
+        """
+        Wraps the get_document_topics from gensim's Nmf.
+        Returns the topic distribution for a given
+        document.
+
+        Args:
+            document (list of (int, int), bow): [description]
+
+        Returns:
+            [type]: [description]
+        """
+        return self.model.get_document_topics(document)
